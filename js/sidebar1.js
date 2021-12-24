2756b3a022297a6fdae0d2d656ca46bb55ef2ef3
3mrld <not3mrld@gmail.com>
2021-12-24 17:11:02 +0700

    Initial commit

---
+++ js/sidebar1.js
@@ -0,0 +1,13 @@
+
+
+
+
+      function OpenNav()
+      {
+        document.getElementById("sidebar").width = "250px";
+        document.getElementById("content").marginLeft = "250px";
+
+      }
+
+
+    
